0be7281505a8c0498ff4768afc586e5b13974398
Artur Diniz Adam <artur.daz@gmail.com>
2019-05-23 17:01:46 -0300

    Fix when 0 change only files duplicates all diffs

--- bash_scripts/lib_generate_fluxo_diff_files.sh
+++ bash_scripts/lib_generate_fluxo_diff_files.sh
@@ -44,11 +44,13 @@ function generate_fluxo_diff_files {
         exit $status
     fi
 
-    local exclude_ignored_diff_args="$(read_fluxo_file "_fluxo_ignore" | xargs -I %% echo "':(exclude)$project_dir/%%'" | tr '\n' ' ')"
-
+    local ignored_files="$(read_fluxo_file "_fluxo_ignore")"
     local change_only_files=$(read_fluxo_file "_fluxo_arquivos_prontos")
-    local change_only_files_diff_arg="$(echo -e "$change_only_files" | xargs -I %% echo "%%" | tr '\n' ' ')"
+    
+    local exclude_ignored_diff_args="$(echo -e "$ignored_files" | xargs -I %% echo "':(exclude)$project_dir/%%'" | tr '\n' ' ')"
     local exclude_change_only_files_diff_arg="$(echo -e "$change_only_files" | xargs -I %% echo "':(exclude)$project_dir/%%'" | tr '\n' ' ')"
+
+    local include_change_only_files_diff_arg="$(echo -e "$change_only_files" | xargs -I %% echo "%%" | tr '\n' ' ')"
 
     IFS=$'\n' branches_array=($(printBranchesOrderedByFluxo))
 
@@ -59,26 +61,28 @@ function generate_fluxo_diff_files {
         local current_branch="${branches_array[i]}"
         local previous_branch="${branches_array[i-1]}"
 
-        local diff_file_name="$tmp_folder/$(printf %0"$digits"d $i)-$current_branch.diff"
-
         local main_diff_command="git diff --minimal '$previous_branch'..'$current_branch' -- $exclude_ignored_diff_args $exclude_change_only_files_diff_arg"
-        local change_only_files_diff_command="git diff --diff-filter=M --minimal '$previous_branch'..'$current_branch' -- $change_only_files_diff_arg"
-        local change_only_files_add_remove_diff_command="git diff --diff-filter=ADR --name-status --minimal '$previous_branch'..'$current_branch' -- $change_only_files_diff_arg"
-
         local main_diff="$(bash -c "$main_diff_command")"
-        local change_only_files_diff="$(bash -c "$change_only_files_diff_command")"
-        local change_only_files_add_remove_diff="$(bash -c "$change_only_files_add_remove_diff_command")"
 
+        local diff_file_name="$tmp_folder/$(printf %0"$digits"d $i)-$current_branch.diff"
 
         echo -e "$main_diff" 2>> /dev/null 1>> "$diff_file_name"
 
-        if [ ! -z "$change_only_files_diff" ]; then
-            echo -e "\\n$change_only_files_diff" 2>> /dev/null 1>> "$diff_file_name"
-        fi
+        if [ ! -z "$change_only_files" ]; then
+            local change_only_files_diff_command="git diff --diff-filter=M --minimal '$previous_branch'..'$current_branch' -- $include_change_only_files_diff_arg"
+            local change_only_files_add_remove_diff_command="git diff --diff-filter=ADR --name-status --minimal '$previous_branch'..'$current_branch' -- $include_change_only_files_diff_arg"
+            
+            local change_only_files_diff="$(bash -c "$change_only_files_diff_command")"
+            local change_only_files_add_remove_diff="$(bash -c "$change_only_files_add_remove_diff_command")"
 
-        if [ ! -z "$change_only_files_add_remove_diff" ]; then
-            echo -e "\\ndiff --fluxo arquivos_curso_add_or_remove" 2>> /dev/null 1>> "$diff_file_name"
-            echo -e "$change_only_files_add_remove_diff" 2>> /dev/null 1>> "$diff_file_name"
+            if [ ! -z "$change_only_files_diff" ]; then
+                echo -e "\\n$change_only_files_diff" 2>> /dev/null 1>> "$diff_file_name"
+            fi
+
+            if [ ! -z "$change_only_files_add_remove_diff" ]; then
+                echo -e "\\ndiff --fluxo arquivos_curso_add_or_remove" 2>> /dev/null 1>> "$diff_file_name"
+                echo -e "$change_only_files_add_remove_diff" 2>> /dev/null 1>> "$diff_file_name"
+            fi
         fi
     done
 
