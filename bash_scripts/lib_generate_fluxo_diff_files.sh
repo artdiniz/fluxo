81a4fdadc50e6a520cf86d2362cf3ff5dbf65f8a
Artur Diniz Adam <artur.daz@gmail.com>
2019-09-06 15:08:00 -0300

    Fix color setup

--- bash_scripts/lib_generate_fluxo_diff_files.sh
+++ bash_scripts/lib_generate_fluxo_diff_files.sh
@@ -25,7 +25,7 @@ function read_fluxo_file {
 }
 
 function generate_fluxo_diff_files {
-    color_setup
+    style_setup
 
     [ -z "$1" ] && local dest_folder="_fluxo_diff_files" || local dest_folder=$1
     local project_dir="$(pwd)"
