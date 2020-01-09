f5e8b43e0594bc98de38d98915e69637909c44f8
Artur Diniz Adam <artur.daz@gmail.com>
2020-01-09 16:20:12 -0200

    install with basher

---
+++ package.sh
@@ -0,0 +1,1 @@
+BINS=bash_scripts/fluxo:bash_scripts/lib_fluxo_doctor.sh:bash_scripts/lib_diff_fluxo_branches.sh:bash_scripts/lib_generate_fluxo_diff_files.sh:bash_scripts/lib_navigate_to_git_repository_root.sh:bash_scripts/lib_parse_fluxo_branches_file.sh:bash_scripts/lib_rebase_fluxo.sh:bash_scripts/lib_show_fluxo.sh:bash_scripts/lib_style.sh:bash_scripts/lib_util.sh:bash_scripts/lib_view.sh
